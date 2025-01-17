9db98afe8a5904304ec7367200c4ece6c89dfe5e
mahendran-indiabees <96326288+mahendran-indiabees@users.noreply.github.com>
2025-01-17 10:20:49 +0530

    MyBuild

--- findBuildStack.py
+++ findBuildStack.py
@@ -12,32 +12,49 @@ STACK_FILES = {
 }
 
 def identify_stack(repo_path):
-    """Identify the stack for a repository from the local file system."""
-    stack = set()
-
-    for root, dirs, files in os.walk(repo_path):
-        print(root)
-        print(dirs)
-        print(files)
-        for file in files:
-            for stack_name, identifiers in STACK_FILES.items():
-                if file in identifiers:
-                    stack.add(stack_name)
+    stack = set()  
+    try:
+        files = os.listdir(repo_path)
+    except FileNotFoundError:
+        print(f"Error: Directory '{repo_path}' or Files not found.")
+        sys.exit(1)
+    except PermissionError:
+        print(f"Error: Permission denied for directory '{repo_path}'.")
+        sys.exit(1)
+    
+    # Check only the files in the root directory
+    for file in files:
+        for stack_name, identifiers in STACK_FILES.items():
+            if file in identifiers:
+                stack.add(stack_name)
     
     return list(stack)
 
-def main():   
-    repo_path = sys.argv[1]
-    print(f"Analyzing repository at: {repo_path}...")
-    stack = identify_stack(repo_path)
-    print("--------------------------------")
-    print(stack)
-    print("--------------------------------")    
-    if len(stack) > 1:
-        stack.insert(0, "Multiple Stacks Detected")  
+def main():
+    # Ensure a path argument is provided
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <repository_path>")
+        sys.exit(1)
     
-    # Print the results
-    print(f"Detected Build stack: {', '.join(stack) if stack else 'No stack detected'}")
+    repo_path = sys.argv[1]
+    print(" ")
+    print(f"Analyzing repository at: {repo_path}...\n")
+    
+    stack = identify_stack(repo_path)
+    print(" ")
+    print("*********************************************")
+    
+    if len(stack) == 0:
+        print("No build stack detected.")
+        sys.exit(1)
+    
+    if len(stack) > 1:
+        print(f"Error: Multiple build stacks detected: {', '.join(stack)}")
+        sys.exit(1)
+    
+    detected_stack = stack[0]
+    print(f"Detected Build Stack: \033[1;32m{detected_stack}\033[0m") 
+    print("*********************************************")
 
 if __name__ == "__main__":
     main()
